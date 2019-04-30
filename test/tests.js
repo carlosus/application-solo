dfdff973f381cfe67b2912b1d2f78fdac2c2e3f4
Carlos Us Santiago <cdus12@gmail.com>
2019-04-30 10:24:59 -0700

    Wrote JS function. Passing TDD test

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,49 @@
 const test = QUnit.test;
+function getOrderEntry(formData) {
+    //convert data to variables
+    const softnessNumber = parseInt(formData.get('softness'));
+
+    //make our object literal
+    const orderEntry = {
+        firstName: formData.get('first-name'),
+        lastName: formData.get('last-name'),
+        size: formData.get('size'),
+        color: formData.get('color'),
+        softnessNumber: softnessNumber,
+        features: formData.getAll('features')
+    };
+
+    return orderEntry;
+
+}
+
 
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const expected = {
+        firstName: 'Carlos',
+        lastName: 'Us',
+        size: 'og',
+        color: 'orange',
+        softnessNumber: 4,
+        features: ['speakers', 'head-massager']
+    };
 
-    //Act 
+    // Set up your parameters and expectations
+    const formData = new FormData();
+    formData.set('first-name', expected.firstName);
+    formData.set('last-name', expected.lastName);
+    formData.set('size', expected.size);
+    formData.set('color', expected.color);
+    formData.set('softness', '4');
+    formData.set('features', expected.features[0]);
+    formData.append('features', expected.features[1]);
+
+     //Act 
     // Call the function you're testing and set the result to a const
+    const orderEntry = getOrderEntry(formData);
 
     //Assert
-    assert.equal(true, false);
+    assert.deepEqual(orderEntry, expected);
 });
