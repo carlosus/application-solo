50cec9d4dea60c4843f3dd7f8626ec34821f92df
Carlos Us Santiago <cdus12@gmail.com>
2019-05-04 20:10:46 -0700

    added anchor link to list

--- src/order-entries.js
+++ src/order-entries.js
@@ -3,14 +3,21 @@ import orderEntryApi from './entry-api.js';
 const tbody = document.getElementById('order-entries');
 
 const orderEntries = orderEntryApi.getAll();
-console.log(orderEntries, 'TEST');
+
 for(let i = 0; i < orderEntries.length; i++) {
     const orderEntry = orderEntries[i];
 
     const tr = document.createElement('tr');
 
+    // create the table cell
     const firstNameCell = document.createElement('td');
-    firstNameCell.textContent = orderEntry.firstName;
+    // create teh anchor link <a>
+    const link = document.createElement('a');
+    const searchParams = new URLSearchParams();
+    searchParams.set('firstName', orderEntry.firstName);
+    link.href = 'entries.html?' + searchParams.toString();
+    link.textContent = orderEntry.firstName;
+    firstNameCell.appendChild(link);
     tr.appendChild(firstNameCell);
 
     const lastNameCell = document.createElement('td');
